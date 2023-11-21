48d74dce4b4a264909fdded785ed393b59f9e00e
Naufal Romero <88795011+romves@users.noreply.github.com>
2023-11-21 11:17:41 +0700

    re adding db utils

---
+++ src/main/java/FoodDelivery/database/DatabaseUtility.java
@@ -0,0 +1,15 @@
+package FoodDelivery.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseUtility {
+    private static final String DB_URL = "jdbc:sqlserver://localhost:1433;databaseName=FoodDeliveryApp;trustServerCertificate=true;encrypt=false";
+    private static final String DB_USER = "sa";
+    private static final String DB_PASSWORD = "Y2FwdGFpbnNpbGVuY2VzbGlwbWU=";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+    }
+}
